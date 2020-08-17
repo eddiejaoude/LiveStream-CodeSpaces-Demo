f98fbb12895fadbbd3aec24590143ee80f151f00
Eddie Jaoude <eddie@jaoudestudios.com>
2020-08-17 17:50:33 +0000

    mongo added to project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const port = 3000;
+
+mongoose.connect('mongodb://mongo:27017/test');
 
 app.get('/', (req, res) => res.send('Hello World, Sam and Eddie in the house!'));
 
